4166786fdc8e36c01c9ee1f789b66a631d33b4d5
alla-coinmerce <alla.maley@coinmerce.io>
2024-11-22 15:05:36 +0100

    feat: login

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,7 @@
 "use server";
-console.log("rawFormData");
+
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 import { z } from "zod";
 import { sql } from "@vercel/postgres";
@@ -103,7 +105,6 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error("Failed to Delete Invoice");
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
@@ -112,5 +113,24 @@ export async function deleteInvoice(id: string) {
     return {
       message: "Failed to delete invoice on the server side!",
     };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
   }
 }
